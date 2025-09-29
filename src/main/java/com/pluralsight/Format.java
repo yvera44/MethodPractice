e2a29d25d01f67bbad1c6909a5757ffd403fa868
YairVera <yairvera44@gmail.com>
2025-09-29 14:17:33 -0500

    Name Format

---
+++ src/main/java/com/pluralsight/Format.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class Format {
+    public static void main(String[] args) {
+
+        formatName("Yair", " Vera");
+
+    }
+    public static void formatName(String first, String last){
+        System.out.println(last + first);
+    }
+}
