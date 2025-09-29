a7f28b407500f00966bba989c020e6a3830aa4eb
YairVera <yairvera44@gmail.com>
2025-09-29 13:53:15 -0500

    HelloMethod

---
+++ src/main/java/com/pluralsight/Hello.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+public class Hello {
+    public static void main(String[] args) {
+
+
+        sayHello();
+        sayGoodbye();
+        sayGoodMorning();
+    }
+    public static void sayHello(){
+        System.out.println("Hello, World!");
+    }
+    public static void sayGoodbye(){
+        System.out.println("Goodbye!");
+    }
+    public static void sayGoodMorning(){
+        System.out.println("Good Morning!");
+    }
+
+
+}
